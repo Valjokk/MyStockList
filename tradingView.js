62ae642a576fdd7cfdf489d702e910bfb243e30e
vkudrjavcevs <valentin.kudrjavcevs@messer-cutting.com>
2020-11-02 13:18:09 +0200

    update.

--- tradingView.js
+++ tradingView.js
@@ -173,3 +173,58 @@ new TradingView.widget(
     "hide_top_toolbar": true,
     "container_id": "tradingview_Grindeks3"
 });
+
+// Olainfarm
+new TradingView.MediumWidget(
+{
+    "symbols": [
+    [
+        "Bayer AG",
+        "OMXRSE:GRD1R|1M"
+    ]
+    ],
+    "chartOnly": false,
+    "width": 500,
+    "height": 300,
+    "locale": "en",
+    "colorTheme": "light",
+    "gridLineColor": "#F0F3FA",
+    "trendLineColor": "#2196F3",
+    "fontColor": "#787B86",
+    "underLineColor": "#E3F2FD",
+    "isTransparent": false,
+    "autosize": false,
+    "container_id": "tradingview_Olainfarm1"
+});
+
+new TradingView.widget(
+{
+    "width": 500,
+    "height": 300,
+    "symbol": "OMXRSE:GRD1R",
+    "interval": "D",
+    "timezone": "Europe/Riga",
+    "theme": "light",
+    "style": "2",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "container_id": "tradingview_Olainfarm2"
+});
+
+new TradingView.widget(
+{
+    "width": 500,
+    "height": 300,
+    "symbol": "OMXRSE:GRD1R",
+    "interval": "M",
+    "timezone": "Europe/Riga",
+    "theme": "light",
+    "style": "2",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "container_id": "tradingview_Olainfarm3"
+});
