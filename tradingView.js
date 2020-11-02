1c92f6debadca2baf5583898f9664e108ca823cc
vkudrjavcevs <valentin.kudrjavcevs@messer-cutting.com>
2020-11-02 13:02:16 +0200

    update

--- tradingView.js
+++ tradingView.js
@@ -1,6 +1,9 @@
 // Advanced chart
 // https://www.tradingview.com/widget/advanced-chart/
 
+// Stock only
+// https://www.tradingview.com/widget/symbol-overview/
+
 // Bayer
 new TradingView.MediumWidget(
 {
@@ -11,7 +14,7 @@ new TradingView.MediumWidget(
     ]
     ],
     "chartOnly": false,
-    "width": 550,
+    "width": 500,
     "height": 300,
     "locale": "en",
     "colorTheme": "light",
@@ -26,7 +29,7 @@ new TradingView.MediumWidget(
 
 new TradingView.widget(
 {
-    "width": 550,
+    "width": 500,
     "height": 300,
     "symbol": "XETR:BAYN",
     "interval": "D",
@@ -42,7 +45,7 @@ new TradingView.widget(
 
 new TradingView.widget(
 {
-    "width": 550,
+    "width": 500,
     "height": 300,
     "symbol": "XETR:BAYN",
     "interval": "M",
@@ -57,63 +60,55 @@ new TradingView.widget(
 });
 
 // BASF SE
+new TradingView.MediumWidget(
+{
+    "symbols": [
+    [
+        "Bayer AG",
+        "XETR:BAYN|1M"
+    ]],
+    "chartOnly": false,
+    "width": 500,
+    "height": 300,
+    "locale": "en",
+    "colorTheme": "light",
+    "gridLineColor": "#F0F3FA",
+    "trendLineColor": "#2196F3",
+    "fontColor": "#787B86",
+    "underLineColor": "#E3F2FD",
+    "isTransparent": false,
+    "autosize": false,
+    "container_id": "tradingview_Basf1"
+});
+
 new TradingView.widget(
-    {
-        "width": 700,
-        "height": 300,
-        "symbol": "XETR:BAS",
-        "interval": "D",
-        "timezone": "Europe/Riga",
-        "theme": "light",
-        "style": "2",
-        "locale": "en",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "hide_top_toolbar": true,
-        "container_id": "tradingview_3"
-    });
+{
+    "width": 700,
+    "height": 300,
+    "symbol": "XETR:BAS",
+    "interval": "D",
+    "timezone": "Europe/Riga",
+    "theme": "light",
+    "style": "2",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "container_id": "tradingview_Basf2"
+});
     
-    new TradingView.widget(
-    {
-        "width": 700,
-        "height": 300,
-        "symbol": "XETR:BAS",
-        "interval": "M",
-        "timezone": "Europe/Riga",
-        "theme": "light",
-        "style": "2",
-        "locale": "en",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "hide_top_toolbar": true,
-        "container_id": "tradingview_4"
-    });
-
-/*
- Stock only
- https://www.tradingview.com/widget/symbol-overview/
-
-            <script type="text/javascript">
-            new TradingView.MediumWidget(
-            {
-                "symbols": [
-                [
-                    "Bayer AG",
-                    "XETR:BAYN|1M"
-                ]
-                ],
-                "chartOnly": false,
-                "width": 600,
-                "height": 300,
-                "locale": "en",
-                "colorTheme": "light",
-                "gridLineColor": "#F0F3FA",
-                "trendLineColor": "#2196F3",
-                "fontColor": "#787B86",
-                "underLineColor": "#E3F2FD",
-                "isTransparent": false,
-                "autosize": false,
-                "container_id": "tradingview_7ba59"
-            });
-            </script>
-*/
+new TradingView.widget(
+{
+    "width": 700,
+    "height": 300,
+    "symbol": "XETR:BAS",
+    "interval": "M",
+    "timezone": "Europe/Riga",
+    "theme": "light",
+    "style": "2",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "hide_top_toolbar": true,
+    "container_id": "tradingview_Basf3"
+});
